1a92d0691edd5e9203c6f4fe01227ad5ce9438b2
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-10-31 18:25:36 -0500

    add info elements and styles

--- src/contact.js
+++ src/contact.js
@@ -3,12 +3,30 @@ const contact = () => {
 
   const div1 = document.createElement('div');
   const h1 = document.createElement('h1');
+  const locationTitle = document.createElement('h3');
+  const locationText = document.createElement('p');
+  const contactTitle = document.createElement('h3');
+  const contactText = document.createElement('p');
+  const finalText = document.createElement('h4');
+
+  locationTitle.textContent = 'Location / Addres: ';
+  locationText.textContent = 'Medellín, Colombia / Cra 33 # 85 Sur 159';
+  contactTitle.textContent = 'Contact / Email: ';
+  contactText.textContent = '+57 321 951 0000 / sushimall@sushi';
+  finalText.textContent = "We'll be happy to answer you!";
+
   h1.textContent = 'Contact';
 
   div1.setAttribute('id', 'main-element');
+  div1.classList.add('text-center', 'd-flex', 'flex-column', 'w-50', 'mx-auto', 'flex-wrap', 'vh-100', 'justify-content-center');
 
-  div1.appendChild(h1);
   container.appendChild(div1);
+  div1.appendChild(h1);
+  div1.appendChild(locationTitle);
+  div1.appendChild(locationText);
+  div1.appendChild(contactTitle);
+  div1.appendChild(contactText);
+  div1.appendChild(finalText);
 };
 
 export default contact;
