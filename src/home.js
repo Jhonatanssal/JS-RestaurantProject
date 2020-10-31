58eab6f69f99958bebd921400e91fe8676620612
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-10-31 14:24:27 -0500

    add images add home page style and elements add home btn functionality

--- src/home.js
+++ src/home.js
@@ -1,14 +1,31 @@
+import homeCover from './img/home-cover.jpg';
+import btnFunctionality from './logic';
+
 const home = () => {
   const cont = document.getElementById('main');
   const div1 = document.createElement('div');
   const h1 = document.createElement('h1');
+  const coverImg = document.createElement('img');
+  const menuBtn = document.createElement('button');
 
+  coverImg.setAttribute('src', homeCover);
+  coverImg.classList.add('img-fluid', 'home-img');
   div1.setAttribute('id', 'main-element');
+  div1.classList.add('mt-2', 'text-center', 'd-flex', 'flex-column', 'w-75', 'mx-auto');
+  h1.classList.add('font-weight-bolder', 'text-danger', 'bg-light', 'p-2', 'rounded', 'col-7', 'mx-auto');
+  menuBtn.classList.add('btn', 'btn-danger', 'menu', 'col-3', 'mx-auto');
+  menuBtn.textContent = 'Menu';
 
-  h1.textContent = 'Home';
+  menuBtn.addEventListener('click', () => {
+    btnFunctionality(menuBtn.classList);
+  });
+
+  h1.textContent = 'Welcome to SushiMall';
 
   cont.appendChild(div1);
+  div1.appendChild(coverImg);
   div1.appendChild(h1);
+  div1.appendChild(menuBtn);
 };
 
 export default home;
--- src/nav.js
+++ src/nav.js
@@ -1,31 +1,35 @@
 import home from './home';
 import btnFunctionality from './logic';
+import mainLogo from './img/logo.png';
 
 const nav = () => {
   const container = document.getElementById('content');
   const nav = document.createElement('nav');
+  const navSpan = document.createElement('span');
   const main = document.createElement('main');
   const content = document.createElement('div');
-  const navItem1 = document.createElement('button');
-  const navItem2 = document.createElement('button');
-  const navItem3 = document.createElement('button');
+  const navItem1 = document.createElement('a');
+  const navItem2 = document.createElement('a');
+  const navItem3 = document.createElement('a');
+  const logo = document.createElement('img');
 
   container.appendChild(nav);
   container.appendChild(main);
   container.appendChild(content);
   main.appendChild(content);
 
-  nav.classList.add('navbar', 'navbar-expand', 'navbar-light', 'bg-light');
+  nav.classList.add('navbar', 'navbar-expand', 'navbar-light', 'bg-danger', 'd-flex', 'justify-content-between', 'shadow-sm');
 
-  navItem1.classList.add('btn', 'nav-btn', 'home');
+  navItem1.classList.add('btn', 'nav-btn', 'home', 'text-white', 'font-weight-bold');
   navItem1.textContent = 'Home';
-  navItem2.classList.add('btn', 'nav-btn', 'menu');
+  navItem2.classList.add('btn', 'nav-btn', 'menu', 'text-white', 'font-weight-bold');
   navItem2.textContent = 'Menu';
-  navItem3.classList.add('btn', 'nav-btn', 'contact', 'contact1');
+  navItem3.classList.add('btn', 'nav-btn', 'contact', 'text-white', 'font-weight-bold');
   navItem3.textContent = 'Contact';
+  logo.setAttribute('src', mainLogo);
+  logo.classList.add('nav-logo');
 
   content.setAttribute('id', 'main');
-  content.classList.add('bg-warning', 'container');
 
   const navBtns = [navItem1, navItem2, navItem3];
 
@@ -38,8 +42,11 @@ const nav = () => {
   });
 
   navBtns.forEach((item) => {
-    nav.appendChild(item);
+    navSpan.appendChild(item);
   });
+
+  nav.appendChild(logo);
+  nav.appendChild(navSpan);
 
   home();
 };
